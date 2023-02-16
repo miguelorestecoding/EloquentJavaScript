66d1d85eed944e9460ea9e9419e84146be9038a9
miguelorestecoding <miguelorestecoding@gmail.com>
2023-02-16 19:29:31 -0300

    primer commit

---
+++ cap02script.js
@@ -0,0 +1,25 @@
+//#
+// for (let n = "#" ; n.length < 7 ; n += "#") {
+//     console.log(n)
+// }
+
+//FIZZ BUZZ
+// for (n = 1; n <=100; n++) {
+//     let output = ""
+//     if (n % 3 == 0) {output += "Fizz"}
+//     if (n % 5 == 0) {output += "Buzz"}
+//     output = (output|| n)
+//     console.log(output)
+// }
+
+//Board
+let size = 8
+let board = ""
+for (y =  0; y < size; y++) {
+for (x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {board += " "}
+    else {board += "#"}
+}
+board += "\n"
+}
+console.log(board)
