da2ec4505e2e3939b8a148abf1cbb6076b5e3da9
miguelorestecoding <miguelorestecoding@gmail.com>
2023-02-16 19:53:00 -0300

    funciones minimo

--- cap02script.js
+++ cap02script.js
@@ -12,7 +12,14 @@
 //     console.log(output)
 // }
 
-//Board
+// for (let n = 1; n <= 100; n++) {
+//     let output = "";
+//     if (n % 3 == 0) output += "Fizz";
+//     if (n % 5 == 0) output += "Buzz";
+//     console.log(output || n);
+//   }
+
+Board
 let size = 8
 let board = ""
 for (y =  0; y < size; y++) {
@@ -22,4 +29,3 @@ for (x = 0; x < size; x++) {
 }
 board += "\n"
 }
-console.log(board)
---
+++ cap03Script.js
@@ -0,0 +1,9 @@
+// MINIMO
+
+// function minimo(a,b) {
+//     if (a<b) return a;
+//     else return b;
+// }
+
+// console.log("El número mínimo es " + minimo(4,6))
+// console.log("El número mínimo es " + minimo(64,8))
