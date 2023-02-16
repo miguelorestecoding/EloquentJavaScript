f3332e8c2f829a7b650794d79fc0b733fa3e51d5
miguelorestecoding <miguelorestecoding@gmail.com>
2023-02-16 20:05:09 -0300

    funciones 2

--- cap03Script.js
+++ cap03Script.js
@@ -7,3 +7,20 @@
 
 // console.log("El número mínimo es " + minimo(4,6))
 // console.log("El número mínimo es " + minimo(64,8))
+
+
+//Recursión
+
+function esPar(n) {
+    if (n == 0) return true;
+    if (n== 1) return false;
+    if (n > 1) return esPar(n - 2);
+    if (n < 0) return esPar(n+2);
+}
+
+console.log(esPar(6))
+console.log(esPar(9))
+console.log(esPar(-6))
+console.log(esPar(-9))
+
+
